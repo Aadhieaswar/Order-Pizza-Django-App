31b0a95c6033df6e16d2702c64fe2229f412ca32
Aadhieaswar <64110005+Aadhieaswar@users.noreply.github.com>
2020-06-19 10:14:23 -0400

    added new paths

--- orders/views.py
+++ orders/views.py
@@ -3,7 +3,19 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    request.session["user"] = "name"
+    request.session.clear()
+    try:
+        var = requet.session["username"]
+    except:
+        var = "session cleared"
     context = {
-    'msg': "Hello!"
+    'msg': var
     }
     return render(request, "orders/index.html", context)
+
+def login(request):
+    return render(request, "orders/login.html", context)
+
+def signup(request):
+    return render(request, "orders/signup.html", context)
