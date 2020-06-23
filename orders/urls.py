81c2e06a9a024ea05d29432569f9cda13723e0f0
Aadhieaswar <64110005+Aadhieaswar@users.noreply.github.com>
2020-06-23 00:26:19 -0400

    added signup features for the webpage

--- orders/urls.py
+++ orders/urls.py
@@ -3,10 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="home"),
     path("login_view", views.login_view, name="login_view"),
     path("signup_view", views.signup_view, name="signup_view"),
-    path("login", views._login, name="_login"),
     path("logout", views._logout, name="_logout"),
-    path("signup", views.signup, name="signup"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.urls import reverse
+from .forms import CreateUser
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -17,27 +20,41 @@ def index(request):
     return render(request, "orders/index.html", context)
 
 def login_view(request):
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            name = user.username
+            request.session["user"] = name
+
+            login(request, user)
+            return HttpResponseRedirect(reverse("home"))
+
+        elif user is None:
+            messages.warning(request, 'Username and/or Password is incorrect')
+            return render(request, "orders/login.html", context_instance=RequestContext)
+
     return render(request, "orders/login.html")
 
 def signup_view(request):
-    return render(request, "orders/signup.html")
 
-def _login(request):
+    form = CreateUser()
 
-    username = request.POST["username"]
-    password = request.POST["password"]
+    if request.method == "POST":
+        form = CreateUser(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
 
-    user = authenticate(request, username=username, password=password)
+    context = {
+    'form': form,
+    }
 
-    if user is not None:
-        name = user.username
-        request.session["user"] = name
-
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
-
-    else:
-        return render(request, "orders/login.html", {'msg': "Invalid Credentials!"})
+    return render(request, "orders/signup.html", context)
 
 def _logout(request):
 
@@ -46,12 +63,7 @@ def _logout(request):
 
     context = {
     'title': 'Session end',
-    'message': 'You have successfull been Logged Out!'
+    'note': 'You have successfull been Logged Out!'
     }
 
     return render(request, "orders/message.html", context)
-
-
-def signup(request):
-
-    return None
