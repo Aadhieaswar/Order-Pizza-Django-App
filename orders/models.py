ef139e3a740ceceda3b4fcde406244d35a70f58b
Aadhieaswar <64110005+Aadhieaswar@users.noreply.github.com>
2020-06-24 00:19:14 -0400

    added some models yet to finish and not migrated yet

--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-#class Location(models.Model):
-#    user_id = models.ForeignKey(Users, on_delete=models.CASCADE, related_name="")
-#    city = models.CharField(max_length=64)
-#    state = models.CharField(max_length=2)
+
+class Pizza(models.Model):
+    pizza = models.CharField(max_length = 64)
+    type = models.CharField(max_length = 64)
+    size_choices = [
+    ('SM', 'Small'),
+    ('LG', 'Large')
+    ]
+    size = models.CharField(max_length = 2, choices = size_choices)
+    cost = models.FloatField(blank = False)
+
+class Toppings(models.Model):
+    topping = models.CharField(max_length = 64)
+
+class Subs(models.Model):
+    sub = models.CharField(max_length = 64)
+    size_choices = [
+    ('SM', 'Small'),
+    ('LG', 'Large')
+    ]
+    size = models.CharField(max_length = 2, choices = size_choices)
+    cost = models.FloatField(blank = False)
+
+class Pastas(models.Model):
+    pasta = models.CharField(max_length = 64)
+    cost = models.FloatField(blank = False)
+
+class Salads(models.Model):
+    salad = models.CharField(max_length = 64)
+    cost = models.FloatField(blank = False)
+
+class DinnerPlatters(models.Model):
+    platter = models.CharField(max_length = 64)
+    size_choices = [
+    ('SM', 'Small'),
+    ('LG', 'Large')
+    ]
+    size = models.CharField(max_length = 2, choices = size_choices)
+    cost = models.FloatField(blank = False)
+
+class Cart(models.Model):
+    user_id = models.ForeignKey(User, on_delete = models.CASCADE)
+
+class Order(models.Model):
+    user_id = models.ForeignKey(User, on_delete = models.CASCADE)
