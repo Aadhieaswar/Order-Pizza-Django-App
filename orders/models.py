0461795d486ae8937bcb3733b0e5e3a58b019fae
Aadhieaswar <64110005+Aadhieaswar@users.noreply.github.com>
2020-07-01 18:21:56 -0400

    updated tables to be easier to read

--- orders/models.py
+++ orders/models.py
@@ -6,15 +6,11 @@ from django.contrib.auth.models import User
 class Pizza(models.Model):
     pizza = models.CharField(max_length = 64)
     type = models.CharField(max_length = 64)
-    size_choices = [
-    ('Small', 'SM'),
-    ('Large', 'LG')
-    ]
-    size = models.CharField(max_length = 5, choices = size_choices)
-    cost = models.FloatField(blank = False)
+    small = models.DecimalField(max_digits = 4, decimal_places = 2)
+    large = models.DecimalField(max_digits = 4, decimal_places = 2)
 
     def __str__(self):
-        return f"{self.type} {self.pizza} pizza - {self.size} - ${self.cost}"
+        return f"{self.type} {self.pizza} pizza - Small: ${self.small} - Large: ${self.large}"
 
 class Topping(models.Model):
     topping = models.CharField(max_length = 64)
@@ -25,55 +21,28 @@ class Topping(models.Model):
 
 class Sub(models.Model):
     sub = models.CharField(max_length = 64)
-    size_choices = [
-    ('Small', 'SM'),
-    ('Large', 'LG')
-    ]
-    size = models.CharField(max_length = 5, choices = size_choices)
-    cost = models.FloatField(blank = False)
-
+    small = models.DecimalField(max_digits = 4, decimal_places = 2)
+    large = models.DecimalField(max_digits = 4, decimal_places = 2)
     def __str__(self):
-        return f"{self.sub} sub - {self.size} - ${self.cost}"
+        return f"{self.sub} sub - Small: ${self.small} - Large: ${self.large}"
 
 class Pasta(models.Model):
     pasta = models.CharField(max_length = 64)
-    cost = models.FloatField(blank = False)
-
+    cost = models.DecimalField(max_digits = 4, decimal_places = 2)
     def __str__(self):
         return f"{self.pasta} - ${self.cost}"
 
 class Salad(models.Model):
     salad = models.CharField(max_length = 64)
-    cost = models.FloatField(blank = False)
+    cost = models.DecimalField(max_digits = 4, decimal_places = 2)
 
     def __str__(self):
         return f"{self.salad} - ${self.cost}"
 
 class DinnerPlatter(models.Model):
     platter = models.CharField(max_length = 64)
-    size_choices = [
-    ('Small', 'SM'),
-    ('Large', 'LG')
-    ]
-    size = models.CharField(max_length = 5, choices = size_choices)
-    cost = models.FloatField(blank = False)
+    small = models.DecimalField(max_digits = 4, decimal_places = 2)
+    large = models.DecimalField(max_digits = 4, decimal_places = 2)
 
     def __str__(self):
-        return f"{self.platter} - {self.size} - ${self.cost}"
-
-#class Cart(models.Model):
-#    user = models.ForeignKey(User, on_delete = models.CASCADE)
-#    date_ordered = models.DateTimeField(auto_now_add=True)
-#    item_choices = [
-#    ('Pizza', 'PIZ'),
-#    ('Sub', 'SUB'),
-#    ('Pasta', 'PAS'),
-#    ('Salad', 'SAL'),
-#    ('DinnerPlatter', 'DPL')
-#    ]
-#    item_type = models.CharField(max_length = 3, choices = item_choices, blank=False, null=False)
-#    item = models.ForeignKey(self.item_type, on_delete = models.SET_NULL)
-
-
-#class Order(models.Model):
-#    user = models.ForeignKey(User, on_delete = models.CASCADE)
+        return f"{self.platter} - Small: ${self.small} - Large: ${self.large}"
